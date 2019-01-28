0202cff20609c6ff01f3b9f9dd85efe02d27b0b1
Moshe <moshemalka@mail.adelphi.edu>
2019-01-28 13:30:37 -0500

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CharactersComponent} from './characters/characters.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/',
+    redirectTo: 'characters'
+  },
+  {
+    path: 'characters/:name',
+    component: CharactersComponent,
+    resolve: {
+      character: CharacterResolver
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
