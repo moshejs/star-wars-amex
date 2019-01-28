d0be742350cb5a95ff8eca7100d83dd8e544e9fc
Moshe <moshemalka@mail.adelphi.edu>
2019-01-28 13:30:15 -0500

    add characters to environment folder

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,25 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  characters: [
+    {
+      'name': 'Luke Skywalker',
+      'url': 'https://swapi.co/api/people/1/'
+    },
+    {
+      'name': 'Darth Vader',
+      'url': 'https://swapi.co/api/people/4/'
+    },
+    {
+      'name': 'Obi-wan Kenobi',
+      'url': 'https://swapi.co/api/people/unknown/'
+    },
+    {
+      'name': 'R2-D2',
+      'url': 'https://swapi.co/api/people/3/'
+    }
+  ]
 };
 
 /*
