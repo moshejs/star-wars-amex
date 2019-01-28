0a6c05d7569a96a3d0b0761b3878973b33ca1197
Moshe <moshemalka@mail.adelphi.edu>
2019-01-28 17:53:39 -0500

    missing deps

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
+//Install express server
 const express = require('express');
-const app = express(),
-  fs = require('fs');
+const path = require('path');
 
-app.get('/*', function(req, res){
-  let filename = __dirname + '/dist/star-wars/' +  req.params[0];
-  fs.exists(filename, function (exists) {
-    if (!exists) {
-      res.sendFile(__dirname + '/dist/star-wars/index.html');
-    } else {
-      res.sendFile( __dirname + '/dist/star-wars/' + req.params[0]);
-    }
-  });
+const app = express();
 
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/star-wars'));
 
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname+'/dist/star-wars/index.html'));
 });
 
-
-// Start the app by listening on the default
-// Heroku port
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
