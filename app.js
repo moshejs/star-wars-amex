95bdbf3d5e068f24cc809010cecb2c07ff19b7a1
Moshe <moshemalka@mail.adelphi.edu>
2019-01-28 17:43:34 -0500

    add server

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express(),
+  fs = require('fs');
+
+app.get('/*', function(req, res){
+  var filename = __dirname + '/dist/star-wars/' +  req.params[0];
+  fs.exists(filename, function (exists) {
+    if (!exists) {
+      res.sendFile(__dirname + '/dist/star-wars/index.html');
+    } else {
+      res.sendFile( __dirname + '/dist/star-wars/' + req.params[0]);
+    }
+  });
+
+
+});
+
+
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
